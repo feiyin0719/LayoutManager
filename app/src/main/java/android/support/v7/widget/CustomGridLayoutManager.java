8e753d2bb3f0f7aa6da1c82ba857ab70ff15a21a
feiyin0719 <582816317@qq.com>
2019-01-22 22:50:33 +0800

    init

---
+++ app/src/main/java/android/support/v7/widget/CustomGridLayoutManager.java
@@ -0,0 +1,406 @@
+package android.support.v7.widget;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.graphics.Rect;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+public class CustomGridLayoutManager extends RecyclerView.LayoutManager {
+    private static final String TAG = "CustomGridLayoutManager";
+
+    public static final int MAX_SPAN_CHANGE_PROCESS = 500;
+    private SparseArray<Rect> itemRects = new SparseArray<>();
+    private int mSpanCount;
+    private int mToSpanCount;
+    public int totalHeight = 0;
+    private int verticalScrollOffset;
+    int[] mCachedBorders;
+    int[] mToCachedBorders;
+    int mSpanChangeProcess;
+    float mOffsetDelta;
+
+    public boolean isSpanChangeMode() {
+        return isSpanChangeMode;
+    }
+
+    private boolean isSpanChangeMode;
+    private ObjectAnimator spanChangeAnim;
+    private Animator.AnimatorListener spanChangeListener = new Animator.AnimatorListener() {
+        @Override
+        public void onAnimationStart(Animator animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animation) {
+            isSpanChangeMode = false;
+            spanChangeAnim = null;
+        }
+
+        @Override
+        public void onAnimationCancel(Animator animation) {
+            isSpanChangeMode = false;
+            spanChangeAnim = null;
+        }
+
+        @Override
+        public void onAnimationRepeat(Animator animation) {
+
+        }
+    };
+
+    public void setSpanChangeProcess(int spanChangeProcess) {
+        if (spanChangeProcess > MAX_SPAN_CHANGE_PROCESS)
+            spanChangeProcess = MAX_SPAN_CHANGE_PROCESS;
+        else if (spanChangeProcess < -MAX_SPAN_CHANGE_PROCESS)
+            spanChangeProcess = -MAX_SPAN_CHANGE_PROCESS;
+        this.mSpanChangeProcess = spanChangeProcess;
+        Log.d(TAG, "spanChangeProcess:" + mSpanChangeProcess);
+        updateSpanChangeProcess();
+    }
+
+    public int getSpanChangeProcess() {
+        return mSpanChangeProcess;
+    }
+
+    public void incChangeProcess() {
+        this.mSpanChangeProcess++;
+        Log.d(TAG, "spanChangeProcess:" + mSpanChangeProcess);
+        updateSpanChangeProcess();
+    }
+
+    public void reduceChangeProcess() {
+        this.mSpanChangeProcess--;
+        updateSpanChangeProcess();
+    }
+
+    private void updateSpanChangeProcess() {
+        if (!isSpanChangeMode) {
+            Log.i(TAG, "must call in spanCountChange mode");
+            return;
+        }
+
+        if (this.mSpanChangeProcess == MAX_SPAN_CHANGE_PROCESS) {
+            mSpanCount += 2;
+            mCachedBorders = null;
+            mToCachedBorders = null;
+            mSpanChangeProcess = 0;
+            verticalScrollOffset += mOffsetDelta;
+            isSpanChangeMode = false;
+            calculateChildrenSite();
+        } else if (this.mSpanChangeProcess == -MAX_SPAN_CHANGE_PROCESS) {
+            mSpanCount -= 2;
+            mCachedBorders = null;
+            mToCachedBorders = null;
+            mSpanChangeProcess = 0;
+            verticalScrollOffset += mOffsetDelta;
+            isSpanChangeMode = false;
+            calculateChildrenSite();
+        } else {
+            if ((mSpanCount <= 2 && this.mSpanChangeProcess < 0) || (mSpanCount > 8 && this.mSpanChangeProcess > 0)) {
+                mSpanChangeProcess = 0;
+                isSpanChangeMode = false;
+                return;
+            }
+            if (mSpanChangeProcess == 0)
+                mToCachedBorders = null;
+            if (mToCachedBorders == null) {
+                if (mSpanChangeProcess > 0)
+                    mToSpanCount = mSpanCount + 2;
+                else if (mSpanChangeProcess < 0)
+                    mToSpanCount = mSpanCount - 2;
+
+                int totalSpace;
+                totalSpace = this.getWidth() - this.getPaddingRight() - this.getPaddingLeft();
+                mToCachedBorders = calculateItemBorders(mToCachedBorders, mToSpanCount, totalSpace);
+                //当发生偏移的时候，因为itemSize发生变化，为了显示当前画面，需要更新滑动距离
+                if (mSpanChangeProcess > 0) {
+                    if (verticalScrollOffset > 0) {
+                        int width = getItemSize(mCachedBorders, mSpanCount);
+                        int toWidth = getItemSize(mToCachedBorders, mToSpanCount);
+                        int count = getItemCount();
+                        int newTotalHeight = count / mToSpanCount * toWidth;
+                        if (count % mToSpanCount != 0)
+                            newTotalHeight += toWidth;
+                        int pheight = getHeight() - getPaddingBottom() - getPaddingTop();
+                        if (verticalScrollOffset < newTotalHeight - pheight || newTotalHeight < pheight) {
+                            int col = verticalScrollOffset / width;
+                            if (verticalScrollOffset % width != 0)
+                                ++col;
+                            pheight /= 2;
+                            col += (pheight / width);
+                            if (pheight % width != 0)
+                                ++col;
+                            int toOffset = col * toWidth - pheight;
+                            mOffsetDelta = (1.0f * toOffset - verticalScrollOffset) / MAX_SPAN_CHANGE_PROCESS;
+                        } else {
+                            mOffsetDelta = (1.0f * newTotalHeight - pheight - verticalScrollOffset) / MAX_SPAN_CHANGE_PROCESS;
+                        }
+                    } else {
+                        mOffsetDelta = 0;
+                    }
+                } else if (mSpanChangeProcess < 0) {
+                   {
+                        int width = getItemSize(mCachedBorders, mSpanCount);
+                        int toWidth = getItemSize(mToCachedBorders, mToSpanCount);
+                        int count = getItemCount();
+                        int newTotalHeight = count / mToSpanCount * toWidth;
+                        if (count % mToSpanCount != 0)
+                            newTotalHeight += toWidth;
+                        int pheight = getHeight() - getPaddingBottom() - getPaddingTop();
+                        int col = verticalScrollOffset / width;
+                        if (verticalScrollOffset % width != 0)
+                            ++col;
+                        pheight /= 2;
+                        col += (pheight / width);
+                        if (pheight % width != 0)
+                            ++col;
+                        int toOffset = col * toWidth - pheight;
+                        mOffsetDelta = (1.0f * toOffset - verticalScrollOffset) / MAX_SPAN_CHANGE_PROCESS;
+
+                    }
+
+                }
+            }
+            verticalScrollOffset += mOffsetDelta;
+
+        }
+        this.requestLayout();
+    }
+
+    public void startSpanChange() {
+        isSpanChangeMode = true;
+        this.mSpanChangeProcess = 0;
+    }
+
+    public void stopSpanChange() {
+        if (isSpanChangeMode && mSpanChangeProcess != 0) {
+            if (spanChangeAnim != null && spanChangeAnim.isRunning()) return;
+            int toProcess = 0;
+            if (mSpanChangeProcess > MAX_SPAN_CHANGE_PROCESS/100*24) {
+                toProcess = MAX_SPAN_CHANGE_PROCESS;
+            } else if (mSpanChangeProcess < - MAX_SPAN_CHANGE_PROCESS/100*24) {
+                toProcess = -MAX_SPAN_CHANGE_PROCESS;
+
+            }
+            spanChangeAnim = ObjectAnimator.ofInt(this, "spanChangeProcess", mSpanChangeProcess, toProcess);
+            spanChangeAnim.setDuration(10/(MAX_SPAN_CHANGE_PROCESS/100)* Math.abs(mSpanChangeProcess - toProcess));
+            spanChangeAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+            spanChangeAnim.start();
+            spanChangeAnim.addListener(spanChangeListener);
+        }
+    }
+
+    public CustomGridLayoutManager(int spanCount) {
+        mSpanCount = spanCount;
+    }
+
+    @Override
+    public RecyclerView.LayoutParams generateDefaultLayoutParams() {
+        return new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+    }
+
+    @Override
+    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
+        if (getItemCount() <= 0 || state.isPreLayout()) {
+            return;
+        }
+        super.onLayoutChildren(recycler, state);
+        detachAndScrapAttachedViews(recycler);
+        calculateChildrenSite();
+        recycleAndFillView(recycler, state);
+    }
+
+    static int[] calculateItemBorders(int[] cachedBorders, int spanCount, int totalSpace) {
+        if (cachedBorders == null || cachedBorders.length != spanCount + 1 || cachedBorders[cachedBorders.length - 1] != totalSpace) {
+            cachedBorders = new int[spanCount + 1];
+        }
+        cachedBorders[0] = 0;
+        int sizePerSpan = totalSpace / spanCount;
+        int sizePerSpanRemainder = totalSpace % spanCount;
+        int consumedPixels = 0;
+        int additionalSize = 0;
+
+        for (int i = 1; i <= spanCount; ++i) {
+            int itemSize = sizePerSpan;
+            additionalSize += sizePerSpanRemainder;
+            if (additionalSize > 0 && spanCount - additionalSize < sizePerSpanRemainder) {
+                itemSize = sizePerSpan + 1;
+                additionalSize -= spanCount;
+            }
+            consumedPixels += itemSize;
+            cachedBorders[i] = consumedPixels;
+        }
+
+        return cachedBorders;
+    }
+
+    private int getItemSize(int[] cachedBorders, int spanCount) {
+        if (spanCount > 1) {
+            return cachedBorders[1] - cachedBorders[0];
+        } else
+            return getWidth() - getPaddingLeft() - getPaddingRight();
+    }
+
+    private void calculateItemBorders(int totalSpace) {
+        this.mCachedBorders = calculateItemBorders(this.mCachedBorders, this.mSpanCount, totalSpace);
+    }
+
+    private void calculateChildrenSite() {
+        totalHeight = 0;
+        int totalSpace;
+        totalSpace = this.getWidth() - this.getPaddingRight() - this.getPaddingLeft();
+        if (mCachedBorders == null) {
+            calculateItemBorders(totalSpace);
+        }
+        totalSpace = getItemSize(mCachedBorders, mSpanCount);
+        Log.i(TAG, "totalSpace:" + totalSpace);
+
+        for (int i = 0; i < getItemCount(); i++) {
+            Rect rect = itemRects.get(i);
+            if (rect == null) {
+                rect = new Rect();
+            }
+            if (i % mSpanCount == 0 && i > 0)
+                totalHeight += totalSpace;
+            rect.set(mCachedBorders[i % mSpanCount], totalHeight, mCachedBorders[i % mSpanCount] + totalSpace, totalHeight + totalSpace);
+            // 保存ItemView的位置信息
+            itemRects.put(i, rect);
+
+        }
+        totalHeight += totalSpace;
+    }
+
+    private void measureChildWithDecorationsAndMargin(View child, int widthSpec, int heightSpec, boolean alreadyMeasured) {
+        android.support.v7.widget.RecyclerView.LayoutParams lp = (android.support.v7.widget.RecyclerView.LayoutParams) child.getLayoutParams();
+        boolean measure;
+        if (alreadyMeasured) {
+            measure = this.shouldReMeasureChild(child, widthSpec, heightSpec, lp);
+        } else {
+            measure = this.shouldMeasureChild(child, widthSpec, heightSpec, lp);
+        }
+        if (measure) {
+            child.measure(widthSpec, heightSpec);
+        }
+    }
+
+    private void recycleAndFillView(RecyclerView.Recycler recycler, RecyclerView.State state) {
+        if (getItemCount() <= 0 || state.isPreLayout()) {
+            return;
+        }
+        // 当前scroll offset状态下的显示区域
+        Rect displayRect = new Rect(0, verticalScrollOffset, getHorizontalSpace(),
+                verticalScrollOffset + getVerticalSpace());
+        int left, top, width, toWidth = 0;
+        width = getItemSize(mCachedBorders, mSpanCount);
+        if (mSpanChangeProcess != 0)
+            toWidth = getItemSize(mToCachedBorders, mToSpanCount);
+        Rect childRect = new Rect();
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            childRect.left = getDecoratedLeft(child);
+            childRect.top = getDecoratedTop(child);
+            childRect.right = getDecoratedRight(child);
+            childRect.bottom = getDecoratedBottom(child);
+            if (mSpanChangeProcess != 0) {
+                left = mToCachedBorders[i % mToSpanCount];
+                top = i / mToSpanCount * toWidth;
+                int process = Math.abs(mSpanChangeProcess);
+                childRect.left = childRect.left + (int) (1.0f * (left - childRect.left) / MAX_SPAN_CHANGE_PROCESS * process);
+                childRect.top = childRect.top + (int) (1.0f * (top - childRect.top) / MAX_SPAN_CHANGE_PROCESS * process);
+                int w = (int) (width + (1.0f * (toWidth - width) / MAX_SPAN_CHANGE_PROCESS) * process);
+                childRect.right = childRect.left + w;
+                childRect.bottom = childRect.top + w;
+            }
+            if (!Rect.intersects(displayRect, childRect)) {
+                //recycle View
+                removeAndRecycleView(child, recycler);
+            }
+        }
+        for (int i = 0; i < getItemCount(); i++) {
+            childRect = itemRects.get(i);
+            if (mSpanChangeProcess != 0) {
+                childRect = new Rect(childRect.left, childRect.top, childRect.right, childRect.bottom);
+                left = mToCachedBorders[i % mToSpanCount];
+                top = i / mToSpanCount * toWidth;
+                int process = Math.abs(mSpanChangeProcess);
+                childRect.left = childRect.left + (int) (1.0f * (left - childRect.left) / MAX_SPAN_CHANGE_PROCESS * process);
+                childRect.top = childRect.top + (int) (1.0f * (top - childRect.top) / MAX_SPAN_CHANGE_PROCESS * process);
+                int w = (int) (width + (1.0f * (toWidth - width) / MAX_SPAN_CHANGE_PROCESS) * process);
+                childRect.right = childRect.left + w;
+                childRect.bottom = childRect.top + w;
+
+            }
+            if (Rect.intersects(displayRect, childRect)) {
+                View itemView = recycler.getViewForPosition(i);
+                int spec = View.MeasureSpec.makeMeasureSpec(childRect.right - childRect.left, View.MeasureSpec.EXACTLY);
+                measureChildWithDecorationsAndMargin(itemView, spec, spec, false);
+
+                addView(itemView);
+
+                layoutDecoratedWithMargins(itemView,
+                        childRect.left,
+                        childRect.top - verticalScrollOffset,
+                        childRect.right,
+                        childRect.bottom - verticalScrollOffset);
+            }
+        }
+        Log.e(TAG, "itemCount = " + getChildCount());
+    }
+
+
+    @Override
+    public boolean canScrollVertically() {
+        //spanchange cannot scroll
+        return !isSpanChangeMode;
+    }
+
+    @Override
+    public int scrollVerticallyBy(int dy, RecyclerView.Recycler recycler, RecyclerView.State state) {
+        detachAndScrapAttachedViews(recycler);
+        //不满一屏无法滚动
+        if (totalHeight < getVerticalSpace()) {
+            recycleAndFillView(recycler, state);
+            return 0;
+        }
+        int travel = dy;
+
+        Log.e(TAG, "dy = " + dy);
+        if (verticalScrollOffset + dy < 0) {
+            travel = -verticalScrollOffset;
+        } else if (verticalScrollOffset + dy > totalHeight - getVerticalSpace()) {
+            travel = totalHeight - getVerticalSpace() - verticalScrollOffset;
+        }
+        verticalScrollOffset += travel;
+        offsetChildrenVertical(-travel);
+        recycleAndFillView(recycler, state);
+        Log.d(TAG, "verticalScrollOffset:" + verticalScrollOffset + " travel:" + travel);
+        return travel;
+    }
+
+    private int getVerticalSpace() {
+
+        return getHeight() - getPaddingBottom() - getPaddingTop();
+    }
+
+    @Override
+    public boolean canScrollHorizontally() {
+
+        return super.canScrollHorizontally();
+    }
+
+    @Override
+    public int scrollHorizontallyBy(int dx, RecyclerView.Recycler recycler,
+                                    RecyclerView.State state) {
+        return super.scrollHorizontallyBy(dx, recycler, state);
+    }
+
+    public int getHorizontalSpace() {
+        return getWidth() - getPaddingLeft() - getPaddingRight();
+    }
+}
---
+++ app/src/main/java/com/iffly/layoutmanager/MainActivity.java
@@ -0,0 +1,145 @@
+package com.iffly.layoutmanager;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CustomGridLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.lang.ref.PhantomReference;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MyAdapter adapter;
+    private CustomGridLayoutManager gridLayoutManager;
+    private ScaleGestureDetector.OnScaleGestureListener onScaleGestureListener=new ScaleGestureDetector.OnScaleGestureListener() {
+        private float prev;
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            Log.d("test","factor:"+detector.getScaleFactor());
+            if(detector.getScaleFactor()>1){
+                prev=detector.getScaleFactor();
+                gridLayoutManager.incChangeProcess();
+
+            }else if(detector.getScaleFactor()<1){
+
+                prev=detector.getScaleFactor();
+                gridLayoutManager.reduceChangeProcess();
+            }
+            return true;
+        }
+
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector detector) {
+            Log.d("test","scale start");
+            gridLayoutManager.startSpanChange();
+            return true;
+        }
+
+        @Override
+        public void onScaleEnd(ScaleGestureDetector detector) {
+            Log.d("test","scale end");
+            gridLayoutManager.stopSpanChange();
+        }
+    };
+    private ScaleGestureDetector scaleGestureDetector;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.recyclerView);
+        gridLayoutManager=new CustomGridLayoutManager(4);
+        adapter=new MyAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        scaleGestureDetector=new ScaleGestureDetector(this,onScaleGestureListener);
+        recyclerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                scaleGestureDetector.onTouchEvent(event);
+                return false;
+            }
+        });
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(){
+                    public void run(){
+                        int i=0;
+                        gridLayoutManager.startSpanChange();
+                        while(i<=150){
+                            ++i;
+                            try {
+                                Thread.sleep(2);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            final int finalI = i;
+                            recyclerView.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    gridLayoutManager.incChangeProcess();
+                                }
+                            });
+                        }
+                        recyclerView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                gridLayoutManager.stopSpanChange();
+                            }
+                        });
+
+
+                    }
+
+
+                }.start();
+            }
+        });
+        //LinearLayoutManager
+
+    }
+    private static class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+
+        private Context context;
+
+        public MyAdapter(Context context) {
+            this.context = context;
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View view= LayoutInflater.from(context).inflate(R.layout.item_layout,viewGroup,false);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+            TextView textView=myViewHolder.itemView.findViewById(R.id.textView);
+            textView.setText(""+i);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 100;
+        }
+
+        public static class MyViewHolder extends RecyclerView.ViewHolder{
+            public MyViewHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+        }
+
+    }
+}
